0f0b9dd5585b3aec8eeacefaead17575c9a2333d
Melvyn Laïly <melvyn.laily@gmail.com>
2015-08-29 15:13:26 +0200

    Introduce an Util class with an IEnumerable<T>.Append(T) extension

---
+++ Scrobbling/Util.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Scrobbling
+{
+    public static class Util
+    {
+        /// <summary>
+        /// Append a single element to the <see cref="IEnumerable{T}"/>.
+        /// </summary>
+        public static IEnumerable<T> Append<T>(this IEnumerable<T> collection, T element)
+        {
+            foreach (var item in collection)
+            {
+                yield return item;
+            }
+            yield return element;
+        }
+    }
+}
