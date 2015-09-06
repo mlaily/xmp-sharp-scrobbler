97953a76d49cd40255b8b09d233ba18f40edefa0
Melvyn Laïly <melvyn.laily@gmail.com>
2015-09-06 16:26:13 +0200

    Refactor managed Scrobble objects creation

--- managed-plugin/SharpScrobbler.cs
+++ managed-plugin/SharpScrobbler.cs
@@ -12,27 +12,35 @@ namespace xmp_sharp_scrobbler_managed
     {
         public string SessionKey { get; set; }
 
-        public void NowPlaying(string artist, string track, string album, int durationMs, string trackNumber, string mbid)
-        {
-            NowPlaying nowPlaying = new NowPlaying(artist, track)
+        private static Scrobble CreateScrobble(string artist, string track, string album, int durationMs, string trackNumber, string mbid, long utcUnixTimestamp = 0)
+            => new Scrobble(artist, track, DateTimeOffset.FromUnixTimeSeconds(utcUnixTimestamp))
             {
                 Album = string.IsNullOrWhiteSpace(album) ? null : album,
                 Duration = durationMs <= 0 ? null : new TimeSpan?(TimeSpan.FromMilliseconds(durationMs)),
                 TrackNumber = string.IsNullOrWhiteSpace(trackNumber) ? null : trackNumber,
                 Mbid = string.IsNullOrWhiteSpace(mbid) ? null : mbid,
             };
-            Track.UpdateNowPlaying(SessionKey, nowPlaying).ContinueWith(x => { });
+
+        public async void NowPlaying(string artist, string track, string album, int durationMs, string trackNumber, string mbid)
+        {
+            NowPlaying nowPlaying = CreateScrobble(artist, track, album, durationMs, trackNumber, mbid);
+            try
+            {
+                var response = await Track.UpdateNowPlaying(SessionKey, nowPlaying);
+                if (!response.Success)
+                {
+                    // TODO: log
+                }
+            }
+            catch (Exception)
+            {
+                // TODO: log
+            }
         }
 
         public void Scrobble(string artist, string track, string album, int durationMs, string trackNumber, string mbid, long utcUnixTimestamp)
         {
-            Scrobble scrobble = new Scrobble(artist, track, DateTimeOffset.FromUnixTimeSeconds(utcUnixTimestamp))
-            {
-                Album = string.IsNullOrWhiteSpace(album) ? null : album,
-                Duration = durationMs <= 0 ? null : new TimeSpan?(TimeSpan.FromMilliseconds(durationMs)),
-                TrackNumber = string.IsNullOrWhiteSpace(trackNumber) ? null : trackNumber,
-                Mbid = string.IsNullOrWhiteSpace(mbid) ? null : mbid,
-            };
+            Scrobble scrobble = CreateScrobble(artist, track, album, durationMs, trackNumber, mbid, utcUnixTimestamp);
             Track.Scrobble(SessionKey, scrobble).ContinueWith(x => { });
         }
 
